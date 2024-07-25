99f3fe43a4ff054bda0d505d66031a078c91df84
Rabindranath Ferreira <ferreiralox@gmail.com>
2024-07-25 15:20:42 +0200

    finish consume riverpod providers for nowPlaying movies

--- lib/presentation/screen/movie/home_screen.dart
+++ lib/presentation/screen/movie/home_screen.dart
@@ -1,22 +1,54 @@
 import 'package:cinemapedia_flutter/config/enviroment/enviroment.dart';
+import 'package:cinemapedia_flutter/presentation/screen/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // final movies = ref.
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: const Text('Home Screen'),
       ),
-      body: Center(
-        child: Text(
-            'Welcome to Cinemapedia Mobile App in Enviroment ${Enviroment.env}, running in port ${Enviroment.port} and ${Enviroment.theMovieDBKey}'),
-      ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final playingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    // add loading
+
+    return ListView.builder(
+      itemCount: playingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = playingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
     );
   }
 }
