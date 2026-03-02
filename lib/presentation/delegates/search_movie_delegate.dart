d4f13abdda1d6fd72f657f720a72aeacd6356095
Rabindranath Ferreira <rabindranath.ferreira@ingka.com>
2026-03-02 23:02:58 +0100

    feat: add movie item after find results

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia_flutter/config/helpers/human_formats.dart';
 import 'package:cinemapedia_flutter/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -108,23 +109,86 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
         final movies = snapshot.data ?? [];
 
         return ListView.builder(
-          itemBuilder: (context, index) {
-            final movie = movies[index];
-            return ListTile(
-              leading:
-                  movie.posterPath.isNotEmpty && movie.posterPath != 'no-poster'
-                  ? Image.network(movie.posterPath, width: 50)
-                  : const Icon(Icons.movie, size: 40),
-              title: Text(movie.title),
-              subtitle: Text(movie.releaseDate.year.toString()),
-              onTap: () {
-                close(context, movie);
-              },
-            );
-          },
           itemCount: movies.length,
+          itemBuilder: (context, index) => _MovieItem(
+            movie: movies[index],
+            onMovieSelected: (context, movie) {
+              // clearStreams();
+              close(context, movie);
+            },
+          ),
         );
       },
+    );
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+  final Function onMovieSelected;
+
+  const _MovieItem({required this.movie, required this.onMovieSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10),
+
+            SizedBox(
+              width: size.width * 0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(movie.title, style: textStyles.titleMedium),
+                  Text(
+                    movie.releaseDate.year.toString(),
+                    style: textStyles.titleSmall,
+                  ),
+                  (movie.overview.length > 100)
+                      ? Text('${movie.overview.substring(0, 100)}...')
+                      : Text(movie.overview),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_half_rounded,
+                        color: Colors.yellow.shade800,
+                      ),
+                      const SizedBox(width: 5),
+                      Text(
+                        HumanFormats.number(movie.voteAverage),
+                        style: textStyles.bodyMedium!.copyWith(
+                          color: Colors.yellow.shade900,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
