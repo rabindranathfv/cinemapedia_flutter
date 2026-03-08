c78956427859584b982de11bf7d89dc88c9482fd
Rabindranath Ferreira <rabindranath.ferreira@ingka.com>
2026-03-08 22:52:57 +0100

    feat: add methods for mapping movies, create a favoriteMoviesProvider and some clean up

--- lib/domain/entities/movie.dart
+++ lib/domain/entities/movie.dart
@@ -14,19 +14,54 @@ class Movie {
   final double voteAverage;
   final int voteCount;
 
-  Movie(
-      {required this.adult,
-      required this.backdropPath,
-      required this.genreIds,
-      required this.id,
-      required this.originalLanguage,
-      required this.originalTitle,
-      required this.overview,
-      required this.popularity,
-      required this.posterPath,
-      required this.releaseDate,
-      required this.title,
-      required this.video,
-      required this.voteAverage,
-      required this.voteCount});
+  Movie({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) => Movie(
+    adult: json['adult'] ?? false,
+    backdropPath: json['backdropPath'] ?? '',
+    genreIds: List<String>.from(json['genreIds'] ?? []),
+    id: json['id'],
+    originalLanguage: json['originalLanguage'] ?? '',
+    originalTitle: json['originalTitle'] ?? '',
+    overview: json['overview'] ?? '',
+    popularity: (json['popularity'] as num?)?.toDouble() ?? 0.0,
+    posterPath: json['posterPath'] ?? '',
+    releaseDate: DateTime.tryParse(json['releaseDate'] ?? '') ?? DateTime(0),
+    title: json['title'] ?? '',
+    video: json['video'] ?? false,
+    voteAverage: (json['voteAverage'] as num?)?.toDouble() ?? 0.0,
+    voteCount: json['voteCount'] ?? 0,
+  );
+
+  Map<String, dynamic> toJson() => {
+    'adult': adult,
+    'backdropPath': backdropPath,
+    'genreIds': genreIds,
+    'id': id,
+    'originalLanguage': originalLanguage,
+    'originalTitle': originalTitle,
+    'overview': overview,
+    'popularity': popularity,
+    'posterPath': posterPath,
+    'releaseDate': releaseDate.toIso8601String(),
+    'title': title,
+    'video': video,
+    'voteAverage': voteAverage,
+    'voteCount': voteCount,
+  };
 }
---
+++ lib/presentation/screen/providers/movies/movies_favorites_provider.dart
@@ -0,0 +1,40 @@
+import 'package:cinemapedia_flutter/domain/entities/movie.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final favoritesMoviesProvider =
+    NotifierProvider<MoviesFavoriteNotifier, Map<String, Movie>>(
+      MoviesFavoriteNotifier.new,
+    );
+
+class MoviesFavoriteNotifier extends Notifier<Map<String, Movie>> {
+  MoviesFavoriteNotifier() : super();
+
+  @override
+  Map<String, Movie> build() {
+    return {};
+  }
+
+  void toggleFavorite(Movie movie) {
+    final String movieId = movie.id.toString();
+    if (state.containsKey(movieId)) {
+      print(
+        'Removing movie from favorites: ${movie.title} (ID: $movieId)',
+      ); // Debug print
+      state = {...state}..remove(movieId);
+    } else {
+      print(
+        'adding movie to favorites: ${movie.title} (ID: $movieId)',
+      ); // Debug print
+      state = {...state, movieId: movie};
+    }
+  }
+
+  bool isFavorite(Movie movie) {
+    final String movieId = movie.id.toString();
+    return state.containsKey(movieId);
+  }
+
+  Future<List<Movie>> getFavoriteMovies() async {
+    return state.values.toList();
+  }
+}
